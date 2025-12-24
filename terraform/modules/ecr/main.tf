b8e24ebf31b64b22859fc816877ff56bb788d82d
sangwon-ryu <fbtkdals2@naver.com>
2025-12-24 19:37:54 +0900

    fix(ecr): kms_key_arn을 optional로 변경
    - encryption_type: AES256도 허용 (기존 ECR 호환성)
    - kms_key_arn: default = null로 optional 변경
    - main.tf: encryption_type 변수 사용으로 수정
    
    기존 AES256 암호화된 ECR 레포지토리와의 호환성 확보

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -31,8 +31,8 @@ resource "aws_ecr_repository" "this" {
   }
 
   encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = var.kms_key_arn
+    encryption_type = var.encryption_type
+    kms_key         = var.encryption_type == "KMS" ? var.kms_key_arn : null
   }
 
   tags = merge(
